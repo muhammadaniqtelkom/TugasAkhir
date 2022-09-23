b4d872c40a89990e9143ddfde207912f3e29aeb5
muhammadaniqtelkom <113883673+muhammadaniqtelkom@users.noreply.github.com>
2022-09-23 15:22:19 +0700

    Update percobaan.py

--- percobaan.py
+++ percobaan.py
@@ -7,7 +7,7 @@ Created on Tue Feb 22 16:08:52 2022
 from libraries import *
 from datetime import datetime
 
-
+#####Import File CSV
 DATA_FOLDER = 'data/'
 filename = 'normal4.csv'
 judul ="emd_normal_4_soft.csv" 
@@ -23,7 +23,7 @@ signal = df.iloc[1:,1][:n_times].values.astype("float64")
 #signal2 = df.iloc[1:,1].values.astype("float64")
 #x = signal;
 
-#denoising using DWT with variant Wavlete
+#####denoising using DWT with variant Wavlete
 for wav in pywt.wavelist():
     try:
         filtered = wavelet_denoising(signal, wavelet=wav, level=1)
@@ -32,7 +32,7 @@ for wav in pywt.wavelist():
     except:
         pass
     
-#ploting for every denoised signal    
+#####ploting for every denoised signal    
     plt.figure(figsize=(10, 6))
     plt.plot(signal, label='Raw')
     plt.plot(filtered, label='Filtered')
@@ -41,7 +41,7 @@ for wav in pywt.wavelist():
     plt.show()
     
     
-#save to array and export to csv 
+######save to array and export to csv 
     judul = wav   
     yable =[]
     print('wavelet: ',wav)
@@ -56,7 +56,7 @@ listtocsv(denoisedata,"EMDdenoisedfile")
 
 
 
-#Denoising using EMD with variant Threshold
+######Denoising using EMD with variant Threshold
 threshold = [0.1 ,0.2 ,0.3 ,0.4 ,0.5 ,0.6 ,0.7 ,0.8 ,0.9]
 threshold1 = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9]
 denoisedata = []
@@ -80,7 +80,7 @@ for i in threshold1:
   
 listtocsv(denoisedata,"EMDdenoisedfile")
 
-#import signal, labeling, and cutting
+######import signal, labeling, and cutting
 folder = 'fitur_DWT'
 file = 'fixedPatient1'
 folderanotasi = 'anotasi'
@@ -89,20 +89,20 @@ n_times = 4665
 # model = DecisionTreeClassifier()
 # svm.SVC()
 # =============================================================================
-ppg_signal, ppg_anotation = annotation_to_ppg_signal_labeled(folder,folderanotasi,file)#labeling raw signal
-ppg_signal, ppg_anotation = annotation_to_ppg_signal_labeled2(folder,folderanotasi,file,n_times)#denoising sinal using DWT and do labeling
-ppg_signal, ppg_anotation = annotation_to_ppg_signal_labeled3(folder,folderanotasi,file,n_times)#denoising sinal using EMD and do labeling
+ppg_signal, ppg_anotation = annotation_to_ppg_signal_labeled(folder,folderanotasi,file)#####labeling raw signal
+ppg_signal, ppg_anotation = annotation_to_ppg_signal_labeled2(folder,folderanotasi,file,n_times)#####denoising sinal using DWT and do labeling
+ppg_signal, ppg_anotation = annotation_to_ppg_signal_labeled3(folder,folderanotasi,file,n_times)#####denoising sinal using EMD and do labeling
 X, y = get_ppg_features(ppg_signal, ppg_anotation, 'bior3.9')#feature Extraction
 
 
-#feature Extraction from specific file
+######feature Extraction from specific file
 ppg_signal1, ppg_anotation1 = annotation_to_ppg_signal_labeled(folder,folderanotasi,'FixedPatient3')
 X, y = get_ppg_features(ppg_signal, ppg_anotation, 'bior3.9')
 
-#export feature to .CSV
+#####export feature to .CSV
 listtocsv(X,"%s.CSV"%(file))
 
-#classification test using all data from feature extraction file.
+######classification test using all data from feature extraction file.
 #path = 'fitur_EMD'
 path = 'fitur_asli'
 all_files = glob.glob(path + "/*.csv")
@@ -131,7 +131,7 @@ for filename1 in all_anotasi:
     ss = df1["event"].values.tolist()
     li1 = li1 + ss
 
-
+#######trainTest, Classifier and Calculate matrix
 labeltest = "pvc"
 X_train, X_test, y_train, y_test = train_test_split(frame, li1, test_size=0.20 , random_state=42)
 z = run_experiment(model, X_train, y_train, X_test, y_test, labeltest)
